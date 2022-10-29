99714d9a64304f962514d0891900e779df607769
autumnschild7 <hikerchickkj@gmail.com>
2022-10-29 17:56:18 -0500

    Forgot to add the StringUtils.cs class.

---
+++ Animals/Utils/StringUtils.cs
@@ -0,0 +1,32 @@
+﻿using Animals;
+using System;
+
+namespace Utils
+{
+    class StringUtils
+    {
+
+        public bool ValidateText(string incomingTxt)
+        {
+            int checkForInt;
+
+            if (int.TryParse(incomingTxt, out checkForInt))
+            {
+                Console.WriteLine("Input cannot be a number. Try again.");
+                return false;
+            }
+            else if (string.IsNullOrWhiteSpace(incomingTxt))
+            {
+                Console.WriteLine("Something went wrong. Please try again.");
+                return false;
+            }
+            else
+                return true;
+        }
+
+        public string UppercaseFirst(string incomingTxt)
+        {
+            return char.ToUpper(incomingTxt[0]) + incomingTxt.Substring(1).ToLower();
+        }
+    }
+}
